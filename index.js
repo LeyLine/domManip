3b262f0d8fe3140e7b0f20ca566f2c8208787e09
Adrian Llerena <a.llerena001@gmail.com>
2018-03-14 00:57:05 -0400

    setting attributes

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hi");
+var paragraph = document.getElementById('paragraph');
+var paragraphId = paragraph.getAttribute('id');
+console.log(paragraphId);
+
+
+var aLink = document.getElementById('google-link');
+aLink.setAttribute('href', 'www.google.com');
